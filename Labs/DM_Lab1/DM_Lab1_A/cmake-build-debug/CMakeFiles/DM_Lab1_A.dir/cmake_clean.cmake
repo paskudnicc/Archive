8273565b0dd6595ddc88df84ac054cbc7ceaff39
paskudnicc <57360472+paskudnicc@users.noreply.github.com>
2019-11-04 19:14:53 +0300

    Add files via upload

---
+++ Labs/DM_Lab1/DM_Lab1_A/cmake-build-debug/CMakeFiles/DM_Lab1_A.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DM_Lab1_A.dir/main.cpp.obj"
+  "DM_Lab1_A.exe"
+  "DM_Lab1_A.exe.manifest"
+  "DM_Lab1_A.pdb"
+  "libDM_Lab1_A.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DM_Lab1_A.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Labs/DM_Lab1/DM_Lab1_E/cmake-build-debug/CMakeFiles/DM_Lab1_E.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DM_Lab1_E.dir/main.cpp.obj"
+  "DM_Lab1_E.exe"
+  "DM_Lab1_E.exe.manifest"
+  "DM_Lab1_E.pdb"
+  "libDM_Lab1_E.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DM_Lab1_E.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
