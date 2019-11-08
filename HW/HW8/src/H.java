6b5c28d63ca559550d7a4429803ebe0158ea53d6
paskudnicc <0088664@mail.ru>
2019-11-08 08:05:36 +0300

    Updated HW8

---
+++ HW/HW8/src/H.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class H {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int [] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = in.nextInt();
+        }
+        int t = in.nextInt();
+    }
+}
