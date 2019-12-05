d7c07ad195a245e3284c6cf2174e7abb12f42eec
paskudnicc <0088664@mail.com>
2019-12-05 19:09:21 +0300

    J Hw 9

--- Labs/LA_Lab2/main.py
+++ Labs/LA_Lab2/main.py
@@ -1,15 +1,15 @@
 import sys
 from math import *
 
-# sys.stdin = open('input.txt', 'r')
-# sys.stdout = open('output.txt', 'w')
+sys.stdin = open('input.txt', 'r')
+sys.stdout = open('output.txt', 'w')
 
 
 class Vector:
-    def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
+    def __init__(self, arr):
+        self.x = arr[0]
+        self.y = arr[1]
+        self.z = arr[2]
 
     def len(self):
         return (dot_product(self, self)) ** 0.5
@@ -22,17 +22,22 @@ class Vector:
     def __str__(self):
         return "(" + str(self.x) + ", " + str(self.y) + ", " + str(self.z) + ")"
 
+    def getx(self):
+        return self.x
+
+    def gety(self):
+        return self.y
+
+    def getz(self):
+        return self.z
+
 
 def vector_sum(a, b):
-    return Vector(a.x + b.x, a.y + b.y, a.z + b.z)
+    return Vector([a.x + b.x, a.y + b.y, a.z + b.z])
 
 
 def vector_mul_by_k(a, k):
-    return Vector(a.x * k, a.y * k, a.z * k)
-
-
-def copy_vector(a):
-    return Vector(a.x, a.y, a.z)
+    return Vector([a.x * k, a.y * k, a.z * k])
 
 
 def dot_product(a, b):
@@ -40,7 +45,7 @@ def dot_product(a, b):
 
 
 def cross_product(a, b):
-    return Vector(a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x)
+    return Vector([a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x])
 
 
 def comb_product(a, b, c):
@@ -51,38 +56,49 @@ def findcos(a, b):
     return dot_product(a, b)/(a.len() * b.len())
 
 
-vx, vy = map(int, input().split())
-ov = Vector(vx, vy, 0)
-ax, ay = map(int, input().split())
-va = Vector(ax, ay, 0)
-mx, my = map(int, input().split())
-vm = Vector(mx, my, 1)
-prm = Vector(mx, my, 0)
-wx, wy = map(int, input().split())
-ow = Vector(wx, wy, 0)
+def vec_check(a, b):
+    return a.getx() * b.gety() - a.gety() * b.getx() >= 0
+
+
+ov = [float(x) for x in input().split()]
+if len(ov) == 2:
+    ov.append(0)
+ov = Vector(ov)
+oa = [float(x) for x in input().split()]
+if len(oa) == 2:
+    oa.append(0)
+oa = Vector(oa)
+vm = [float(x) for x in input().split()]
+prm = vm.copy()
+if len(vm) == 2:
+    vm.append(1)
+    prm.append(0)
+prm[2] = 0
+vm = Vector(vm)
+prm = Vector(prm)
+ow = [float(x) for x in input().split()]
+if len(ow) == 2:
+    ow.append(0)
+ow = Vector(ow)
 
 vw = vector_sum(vector_mul_by_k(ov, -1), ow)
 
-beta = degrees(acos(findcos(vw, va)))
+beta = 90 - degrees(acos(findcos(vw, oa)))
 
-if beta > 90:
-    beta = beta - 90
-else:
-    beta = 90 - beta
+gamma = asin(prm.len()/vm.len())
+if not vec_check(oa, prm):
+    gamma *= -1
 
-if prm.len() != 0:
-    gamma = degrees(acos(findcos(vm, prm)))
-else:
-    gamma = 90
-
-if beta > 60 or gamma < 30:
+if abs(beta) > 60 or gamma > 60:
     print(0)
-elif comb_product(vw, va, Vector(0, 0, 1)) > 0:
-    print(-1)
 else:
-    print(1)
+    if vec_check(oa, vw):
+        print(1)
+    else:
+        print(-1)
+    print(beta)
+    print(gamma)
+    print("Let's try english now")
 
-
-print(round(beta, 2))
-print(round(90 - gamma, 2))
-print("Я стреляю сигареты раздаю бомжам")
+sys.stdin.close()
+sys.stdout.close()
