3cc68c64646f13cf9abbdf7c4fdbd88ea379920d
paskudnicc <0088664@mail.ru>
2019-12-20 23:20:55 +0300

    Laba Algo

--- Labs/Algo_Lab3/G.py
+++ Labs/Algo_Lab3/G.py
@@ -1,37 +1,45 @@
 skobki = input()
-
 lenn = len(skobki)
 inf = 99999999999999
+
 dp = [None] * lenn
+p = [None] * lenn
 for i in range(lenn):
     dp[i] = [0] * lenn
+    p[i] = [0] * lenn
 
 for r in range(lenn):
     dp[r][r] = 1
     for l in range(r - 1, -1, -1):
         if skobki[l] == '{' and skobki[r] == '}' or \
-        skobki[l] == '(' and skobki[r] == ')' or \
-        skobki[l] == '[' and skobki[r] == ']':
+                skobki[l] == '(' and skobki[r] == ')' or \
+                skobki[l] == '[' and skobki[r] == ']':
             min = dp[l + 1][r - 1]
+            # если скобки можно удалить
         else:
             min = inf
+        mink = -1
         for k in range(l, r):
             if dp[k + 1][r] + dp[l][k] < min:
                 min = dp[k + 1][r] + dp[l][k]
-                # bestDiv = k
+                mink = k
+        # массив с разбиениями
+        p[l][r] = mink
         dp[l][r] = min
 
-    print(*dp, sep='\n')
-    print()
-# x in range l, r
-# dp[l][r] = dp[x + 1][r] + dp[l][x]
+answ = ""
 
-# 1 0 1 2 1 2 1 2
-# { } ) ( ) [ ] )
-# ^ v ^ ^ v ^ v ^
-# + + - + + + + -
+def find(left, right):
+    if left >= right:
+        return ''
+    if dp[left][right] == 0:
+        return skobki[left:right + 1]
+    if dp[left][right] - 1 != r - l:
+        if p[left][right] == -1:
+            # better not to remove l and r brackets
+            return skobki[left] + find(left + 1, right - 1) + skobki[right]
+        else:
+            return find(left, p[left][right]) + find(p[left][right] + 1, right)
 
-# 1 2 3 4 5 4 3 2 1 0
-# ( ( ( ( ( ) ) ) ) )
-# ^ ^ ^ ^ ^ v v v v v
-# + + + + + + + + + +
+
+print(find(0, lenn - 1))
